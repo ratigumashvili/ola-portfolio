a2e2087189b5910dffd375c17eea5e85d673df62
Rati <ratigumashvili@gmail.com>
2022-11-03 17:56:52 +0400

    Finished filters

--- src/components/ui/FilterRecords.jsx
+++ src/components/ui/FilterRecords.jsx
@@ -1,17 +1,30 @@
-const FilterRecords = ({ handleFilter }) => {
+const FilterRecords = ({ handleFilter, selected }) => {
   return (
     <div className="filters">
       <label htmlFor="all">All</label>
-      <input type="radio" name="choose" id="all" onChange={handleFilter} />
+      <input
+        type="radio"
+        name="choose"
+        id="all"
+        onChange={handleFilter}
+        checked={selected === "all"}
+      />
       <label htmlFor="production">Production</label>
       <input
         type="radio"
         name="choose"
         id="production"
         onChange={handleFilter}
+        checked={selected === "production"}
       />
       <label htmlFor="practice">Practice</label>
-      <input type="radio" name="choose" id="practice" onChange={handleFilter} />
+      <input
+        type="radio"
+        name="choose"
+        id="practice"
+        onChange={handleFilter}
+        checked={selected === "practice"}
+      />
     </div>
   );
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,29 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { projectsData } from "../data";
 import Card from "../components/ui/Card";
 import FilterRecords from "../components/ui/FilterRecords";
 
 const Projects = () => {
   const [initialData, setInitialData] = useState(projectsData);
-  //   const [checked, setChecked] = useState(true)
+  const [selected, setSelected] = useState("all");
+
+  useEffect(() => {
+    setSelected("all");
+  }, []);
   const handleFilter = (e) => {
     if (e.target.id === "all") {
       setInitialData(projectsData);
+      setSelected("all");
     }
     if (e.target.id === "production") {
       const newArr = projectsData.filter((item) => item.stage === "Production");
       setInitialData(newArr);
+      setSelected("production");
     }
     if (e.target.id === "practice") {
       const newArr = projectsData.filter((item) => item.stage === "Practice");
       setInitialData(newArr);
+      setSelected("practice");
     }
   };
   return (
     <div className="page">
       <div className="title-holder">
         <h1>Projects</h1>
-        <FilterRecords handleFilter={handleFilter} />
+        <FilterRecords handleFilter={handleFilter} selected={selected} />
       </div>
       <div className="projects">
         {initialData.map((item) => (
