44d7dde78f15c38b8e4df7c807f887601144b56a
Rati <ratigumashvili@gmail.com>
2022-11-03 21:50:13 +0400

    grabed bio from data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import { about } from "../data";
+
 const Home = () => {
   return (
     <div className="page">
@@ -9,12 +11,10 @@ const Home = () => {
       <h2 className="greetnig-title">
         I'm <span>Rati</span> Gumashvili
       </h2>
-      <p className="greetnig-text">
-        Tbilisi based front-end developer from Georgia. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Et quaerat qui corrupti beatae
-        quibusdam dolorem reiciendis labore facere distinctio fuga, similique
-        accusamus laudantium nam harum! Tempore accusamus suscipit sit aliquid.
-      </p>
+      <div
+        className="greetnig-text"
+        dangerouslySetInnerHTML={{ __html: about?.bio.toString() }}
+      ></div>
       <Link to="/projects" className="cta">
         View my projects
       </Link>
